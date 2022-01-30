3867c9e15f6c7ab8c28bf1f4aa645c84ce4b6f8f
Daniel Dietrich <cafebab3@gmail.com>
2022-01-30 02:12:28 +0100

    Added web import (#6)

--- src/web-import.js
+++ src/web-import.js
@@ -1,48 +1,45 @@
 // @ts-check
+
 import { extractBaseUrl, createUrl } from './urls';
 
 /**
- * High-level <web-import> functionality.
- * The contents of the <web-import> element are imported and inserted into the DOM.
+ * High-level web import functionality.
+ * The contents of the web import element are imported and inserted into the DOM.
  * There is no internal caching, multiple imports of the same URL are all loaded.
  * 
  * @param {string} baseUrl the base path
- * @param {WithQuerySelectorAll} element an element to process
- * @param {?boolean} debug whether to enable debug mode
- * 
- * @typedef {Node & { querySelectorAll: (selector: any) => NodeListOf<any> }} WithQuerySelectorAll
+ * @param {Element | DocumentFragment} element an element to process
+ * @param {import('.').ComponentProcessor} componentProcessor
+ * @returns {Promise<void>}
  */
-export async function webImport(baseUrl, element, debug) {
+export async function webImport(baseUrl, element, componentProcessor) {
   const elements = element.querySelectorAll('web-import');
-  await Promise.allSettled(Array.from(elements).map(async (el) => {
+  await Promise.allSettled([...elements].map(async (el) => {
     if (!el.getAttribute('status')) {
       el.setAttribute('status', 'loading');
       try {
         const link = el.getAttribute('src');
         const resourceUrl = createUrl(baseUrl, link);
-        // TODO: add options to fetch, like crossorigin, credentials, mode, cache, redirect, referrer, integrity, keepalive, window, etc.
-        const response = await fetch(resourceUrl + '?timestamp=' + Date.now()); // TODO(@@dd): remove timestamp
+        const response = await fetch(resourceUrl);
         if (response.ok) {
           const content = await response.text();
           const fragment = document.createRange().createContextualFragment(content);
           const newBaseUrl = extractBaseUrl(resourceUrl);
           // recurse before connecting the fragment to the live DOM
-          await webImport(newBaseUrl, fragment, debug);
-          if (debug) {
-            el.innerHTML = ''; // reset content
-            el.appendChild(fragment);
-          } else {
-            el.parentNode.replaceChild(fragment, el);
-          }
-          el.setAttribute('status', 'ok');
+          await componentProcessor(newBaseUrl, fragment);
+          el.parentNode.replaceChild(fragment, el);
         } else {
-          el.setAttribute('status', 'error');
-          el.textContent = `${response.status} ${response.statusText}`;
+          logError(el, `${response.status} ${response.statusText}`);
         }
       } catch (error) {
-        el.setAttribute('status', 'error');
-        el.textContent = error.message;
+        logError(el, error);
       }
     }
   }));
+}
+
+function logError(element, error) {
+  console.error("[candid] web import error", element, error);
+  element.setAttribute('status', 'error');
+  element.setAttribute('message', JSON.stringify(error));
 }
