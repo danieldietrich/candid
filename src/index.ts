aeead4911075cc0a8b2de26828d1659e6fc6ef47
Daniel Dietrich <cafebab3@gmail.com>
2022-02-06 19:12:39 +0100

    Reverted HTML API and hid web-component and web-import (#34)

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ import { WebComponent } from "./web-component";
 import { WebImport } from "./web-import";
 
 customElements.define('web-import', WebImport);
-customElements.define('web-component', WebComponent, { extends: 'template' });
+customElements.define('web-component', WebComponent);
